654bb9e764a22f88c2d16c8b15981b6264468149
D3eviee <80790777+D3eviee@users.noreply.github.com>
2025-10-20 00:01:21 +0200

    Implement Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
-
+import { footerLinks }  from "../constants"
 const Footer = () => {
   return (
-    <div>Performance</div>
+    <footer>
+      <div className="info">
+        <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000 000 000</p>
+        <img src="/logo.svg" alt="Apple logo"/>
+      </div>
+      
+      <hr/>
+      
+      <div className="links">
+        <p>Copywright &copy 2025 Apple Inc. All rights reserved</p>
+        
+        <ul>
+          { footerLinks.map(({label, link}) => 
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          )}
+        </ul>
+      </div>  
+    </footer>
   )
 }
 
